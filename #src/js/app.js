3cbfbef74b6bda48269d7ca31e61ff89d8facae9
Islam <isxmad@gmail.com>
2023-04-01 03:57:17 +0300

    f

--- #src/js/app.js
+++ #src/js/app.js
@@ -17,4 +17,41 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
 
+    const goToSub = () => {
+        const btn = document.querySelector('.config__head_ex');
+        const configPage = document.querySelector('.config');
+        const subPage = document.querySelector('.spricing');
+        const back = document.querySelector('.spricing__top_label_gf');
+
+        btn.addEventListener('click', () => {
+            configPage.classList.remove('active');
+            subPage.classList.add('active')
+        })
+
+        back.addEventListener('click', () => {
+            configPage.classList.add('active')
+            subPage.classList.remove('active')
+        })
+    }
+
+    const goToSettings = () => {
+        const btn = document.querySelector('.spricing__top_toggle_btn');
+        const subPage = document.querySelector('.spricing');
+        const settingsPage = document.querySelector('.settings');
+        const back = document.querySelector('.settings__action_item-bc');
+
+        
+        btn.addEventListener('click', () => {
+            subPage.classList.remove('active')
+            settingsPage.classList.add('active')
+        })
+
+        back.addEventListener('click', () => {
+            settingsPage.classList.remove('active')
+            subPage.classList.add('active')
+        })
+    }
+
+    goToSub()
+    goToSettings()
 })  
